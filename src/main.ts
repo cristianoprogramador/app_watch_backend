b8f25a82ce84933ef457cf53a2d4efa06c96bc66
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2024-05-20 17:31:04 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Application Monitoring API')
+    .setDescription('API documentation for the application monitoring service')
+    .setVersion('1.0')
+    .addTag('monitoring')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
