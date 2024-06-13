a7ae8244ed98cfc7a0b59b9ede8d373139830039
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2024-06-13 14:43:44 -0300

    feat: disable swagger

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import { CustomExceptionFilter } from "./error-logs/custom-exception.filter";
 import { ErrorLogsService } from "./error-logs/error-logs.service";
 import * as express from "express";
 import { LanguageMiddleware } from "./common/middlewares/language.middleware";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,35 +29,37 @@ async function bootstrap() {
     }
   );
 
-  const config = new DocumentBuilder()
-    .setTitle("App Watch API")
-    .setDescription(
-      "API documentation for the application monitoring called App Watch API"
-    )
-    .setVersion("1.0")
-    .addBearerAuth({ in: "header", type: "http" })
-    .build();
+  if (process.env.ENABLE_SWAGGER === "true") {
+    const config = new DocumentBuilder()
+      .setTitle("App Watch API")
+      .setDescription(
+        "API documentation for the application monitoring called App Watch API"
+      )
+      .setVersion("1.0")
+      .addBearerAuth({ in: "header", type: "http" })
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document, {
-    swaggerOptions: {
-      tagsSorter: "alpha",
-      defaultModelsExpandDepth: -1,
-      operationsSorter: function (a: any, b: any) {
-        const order = {
-          post: "0",
-          get: "1",
-          put: "2",
-          delete: "3",
-          patch: "4",
-        };
-        return (
-          order[a.get("method")].localeCompare(order[b.get("method")]) ||
-          a.get("path").localeCompare(b.get("path"))
-        );
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document, {
+      swaggerOptions: {
+        tagsSorter: "alpha",
+        defaultModelsExpandDepth: -1,
+        operationsSorter: function (a: any, b: any) {
+          const order = {
+            post: "0",
+            get: "1",
+            put: "2",
+            delete: "3",
+            patch: "4",
+          };
+          return (
+            order[a.get("method")].localeCompare(order[b.get("method")]) ||
+            a.get("path").localeCompare(b.get("path"))
+          );
+        },
       },
-    },
-  });
+    });
+  }
 
   const errorLogsService = app.get(ErrorLogsService);
   app.useGlobalFilters(new CustomExceptionFilter(errorLogsService));
